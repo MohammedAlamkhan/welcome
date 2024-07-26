38de3e9c30dc3f2517cdaac20c607d7bba14736a
MohammedAlamKhan <mohammedalamkhan6@gmail.com>
2024-07-26 10:13:09 +0530

    card poc5

---
+++ script.js
@@ -0,0 +1,19 @@
+const carousel = document.querySelector('.carousel-container');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', (e) => {
+        e.preventDefault();
+        const link = card.getAttribute('data-link');
+
+        // Prevent scrolling
+        document.body.style.overflow = 'hidden';
+        carousel.style.overflow = 'hidden';
+
+        card.classList.add('flipping');
+
+        setTimeout(() => {
+            window.location.href = link;
+        }, 1000);
+    });
+});
