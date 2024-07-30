8a61c352e29f074f10ea377f269eb4f95e243be6
MohammedAlamKhan <mohammedalamkhan6@gmail.com>
2024-07-30 18:37:43 +0530

    fix 6

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/welcome/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered successfully:', registration.scope);
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed:', error);
-        });
-    });
-}
-
 const carousel = document.querySelector('.carousel-container');
 const cards = document.querySelectorAll('.card');
 
